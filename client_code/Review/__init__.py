d98087e5b0a2b3252fd89bab24e1bbae3ab75a20
sentulmari18@gmail.com <sentulmari18@gmail.com>
2024-09-09 07:04:05 +0000

    Edited Review and Review.ItemTemplate5

--- client_code/Review/__init__.py
+++ client_code/Review/__init__.py
@@ -10,30 +10,21 @@ class Review(ReviewTemplate):
     # Call the method to load images
     self.load_undetected_images()
 
-######TEST
-
-
-
-
-
-
-
-
-def load_undetected_images(self):
-    try:
-        # Call the server function to get images
-        print("Inside the load_undetected_images function in Anvil Form")
-        image_data_list = anvil.server.call('get_images')
-
-        # Check the status of the response
-        if image_data_list['status'] == 'success':
-          # Set the repeating panel with the list of image data
-          self.repeating_panel_1.items = image_data_list['data']
-        else:
-            # Handle errors, e.g., show an alert
-            alert(image_data_list['message'])
-    except Exception as e:
-        alert(f"An error occurred: {e}")
-
+  def load_undetected_images(self):
+      try:
+          # Call the server function to get images
+          print("Inside the load_undetected_images function in Anvil Form")
+          image_data_list = anvil.server.call('get_images')
+  
+          # Check the status of the response
+          if image_data_list['status'] == 'success':
+            # Set the repeating panel with the list of image data
+            self.repeating_panel_1.items = image_data_list['data']
+          else:
+              # Handle errors, e.g., show an alert
+              alert(image_data_list['message'])
+      except Exception as e:
+          alert(f"An error occurred: {e}")
+  
 
 
