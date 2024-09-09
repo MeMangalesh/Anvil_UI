f7030104265b6db5921c4ece9b77a4cdbf9cf5bb
sentulmari18@gmail.com <sentulmari18@gmail.com>
2024-09-09 06:45:19 +0000

    Edited Review and Review.ItemTemplate5

--- client_code/Review/__init__.py
+++ client_code/Review/__init__.py
@@ -4,22 +4,36 @@ import anvil.server
 
 class Review(ReviewTemplate):
   def __init__(self, **properties):
+    # Any code you write here will run before the form opens.
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    # Call the method to load images
+    self.load_undetected_images()
 
-    # Any code you write here will run before the form opens.
+######TEST
+
+
+
+
+
+
+
 
 def load_undetected_images(self):
-  # Call the server function to get images
-    image_data_list = anvil.server.call('get_images')
+    try:
+        # Call the server function to get images
+        print("Inside the load_undetected_images function in Anvil Form")
+        image_data_list = anvil.server.call('get_images')
 
-  # Check the status of the response
-    if image_data_list['status'] == 'success':
-      # Set the repeating panel with the list of image data
-      self.repeating_panel_1.items = image_data_list['data']
-    else:
-        # Handle errors, e.g., show an alert
-        alert(image_data_list['message'])
+        # Check the status of the response
+        if image_data_list['status'] == 'success':
+          # Set the repeating panel with the list of image data
+          self.repeating_panel_1.items = image_data_list['data']
+        else:
+            # Handle errors, e.g., show an alert
+            alert(image_data_list['message'])
+    except Exception as e:
+        alert(f"An error occurred: {e}")
 
 
 
