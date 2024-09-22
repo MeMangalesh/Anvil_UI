1d95fc6ddd1b23ce43f27718163efd34233db9fc
sentulmari18@gmail.com <sentulmari18@gmail.com>
2024-09-22 23:11:30 +0000

    Created Review_Img_Admin; Deleted Form2

--- client_code/Review_Img_Admin/__init__.py
+++ client_code/Review_Img_Admin/__init__.py
@@ -1,9 +1,9 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import Review_Img_AdminTemplate
 from anvil import *
 import plotly.graph_objects as go
 import anvil.server
 
-class Form2(Form2Template):
+class Review_Img_Admin(Review_Img_AdminTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
