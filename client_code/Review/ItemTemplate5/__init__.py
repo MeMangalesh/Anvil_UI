14f6a65ffc9b6097ac75a0a6c96c3647994faf6d
sentulmari18@gmail.com <sentulmari18@gmail.com>
2024-10-01 00:30:27 +0000

    Edited Review, Review.ItemTemplate5 and theme.css

--- client_code/Review/ItemTemplate5/__init__.py
+++ client_code/Review/ItemTemplate5/__init__.py
@@ -1,9 +1,28 @@
 from ._anvil_designer import ItemTemplate5Template
 from anvil import *
-from . import ImagePopup  # Import the new ImagePopup form
+# from . import ImagePopup  # Import the new ImagePopup form
 import anvil.server
 
 class ItemTemplate5(ItemTemplate5Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Set the image source, filename, and id using the item passed to the template
+    self.image_display.source = "data:image/png;base64," + self.item['image_base64']
+    self.label_id.text = str(self.item['id'])  # Assuming you have another Label component for the ID
+    # Assuming `dt_processed` is included in self.item
+    if 'processed_dt' in self.item and self.item['processed_dt']:
+        self.label_processed_dt.text = self.item['processed_dt']
+    else:
+        self.label_processed_dt.text = "N/A"  # Handle missing date if necessary
+
+    # Assign the CSS class 'enlarge-on-hover' to the image for hover effect
+    # self.image_display.role = 'enlarge-on-hover'
+    # print(f"Role set for image: {self.image_display.role}")
+#######################################
+  # code above replaced with below
+#######################################
   # def __init__(self, **properties):
   #   # Set Form properties and Data Bindings.
   #   self.init_components(**properties)
@@ -12,36 +31,22 @@ class ItemTemplate5(ItemTemplate5Template):
   #   self.image_display.source = "data:image/png;base64," + self.item['image_base64']
   #   self.label_id.text = str(self.item['id'])  # Assuming you have another Label component for the ID
 
-  #   # Assign the CSS class 'enlarge-on-hover' to the image for hover effect
+  #   # Assign the CSS class 'enlarge-on-hover' to the image
   #   self.image_display.role = 'enlarge-on-hover'
-  #   print(f"Role set for image: {self.image_display.role}")
-#######################################
-  # code above replaced with below
-#######################################
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
 
-    # Set the image source, filename, and id using the item passed to the template
-    self.image_display.source = "data:image/png;base64," + self.item['image_base64']
-    self.label_id.text = str(self.item['id'])  # Assuming you have another Label component for the ID
+  #   # Bind the hover event
+  #   self.image_display.bind("mouseenter", self.show_popup)
+  #   self.image_display.bind("mouseleave", self.hide_popup)
 
-    # Assign the CSS class 'enlarge-on-hover' to the image
-    self.image_display.role = 'enlarge-on-hover'
+  #   self.popup = ImagePopup()  # Create an instance of the popup
 
-    # Bind the hover event
-    self.image_display.bind("mouseenter", self.show_popup)
-    self.image_display.bind("mouseleave", self.hide_popup)
+  #   def show_popup(self, **event_args):
+  #       # Set the image source in the popup
+  #       self.popup.image.source = self.image_display.source
+  #       self.popup.show()  # Display the popup
 
-    self.popup = ImagePopup()  # Create an instance of the popup
-
-    def show_popup(self, **event_args):
-        # Set the image source in the popup
-        self.popup.image.source = self.image_display.source
-        self.popup.show()  # Display the popup
-
-    def hide_popup(self, **event_args):
-        self.popup.hide()  # Hide the popup
+  #   def hide_popup(self, **event_args):
+  #       self.popup.hide()  # Hide the popup
   #################################
   
   def button_save_click(self, **event_args):
