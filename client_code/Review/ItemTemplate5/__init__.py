0ae6e41dce9ca67b12565481eca7628e31fc6fed
sentulmari18@gmail.com <sentulmari18@gmail.com>
2024-10-01 08:36:33 +0000

    Edited Review.ItemTemplate5

--- client_code/Review/ItemTemplate5/__init__.py
+++ client_code/Review/ItemTemplate5/__init__.py
@@ -15,50 +15,7 @@ class ItemTemplate5(ItemTemplate5Template):
     self.label_id.text = str(self.item['id'])  # Assuming you have another Label component for the ID
     # self.label_processed_dt.text = str(self.item['processed_dt'])
     
-    # # Handle `processed_dt` - format as a string if present, otherwise set to "N/A"
-    # if 'processed_dt' in self.item and self.item['processed_dt']:
-    #   # # Format the date if it's a datetime object, otherwise convert to string
-    #   # if isinstance(self.item['processed_dt'], datetime):
-    #   #     formatted_date = self.item['processed_dt'].strftime("%Y-%m-%d")
-    #   # else:
-    #   #     formatted_date = str(self.item['processed_dt'])  # In case it's already a string
-    #   # print(f"Processed date: {formatted_date}")
-    #   self.label_processed_dt.text = self.item['processed_dt']
-    # else:
-    #     self.label_processed_dt.text = "N/A"  # Handle missing date if necessary
-
-    # Assign the CSS class 'enlarge-on-hover' to the image for hover effect
-    # self.image_display.role = 'enlarge-on-hover'
-    # print(f"Role set for image: {self.image_display.role}")
-#######################################
-  # code above replaced with below
-#######################################
-  # def __init__(self, **properties):
-  #   # Set Form properties and Data Bindings.
-  #   self.init_components(**properties)
-
-  #   # Set the image source, filename, and id using the item passed to the template
-  #   self.image_display.source = "data:image/png;base64," + self.item['image_base64']
-  #   self.label_id.text = str(self.item['id'])  # Assuming you have another Label component for the ID
-
-  #   # Assign the CSS class 'enlarge-on-hover' to the image
-  #   self.image_display.role = 'enlarge-on-hover'
-
-  #   # Bind the hover event
-  #   self.image_display.bind("mouseenter", self.show_popup)
-  #   self.image_display.bind("mouseleave", self.hide_popup)
-
-  #   self.popup = ImagePopup()  # Create an instance of the popup
-
-  #   def show_popup(self, **event_args):
-  #       # Set the image source in the popup
-  #       self.popup.image.source = self.image_display.source
-  #       self.popup.show()  # Display the popup
-
-  #   def hide_popup(self, **event_args):
-  #       self.popup.hide()  # Hide the popup
-  #################################
-  
+ 
   def button_save_click(self, **event_args):
     checkbox = self.check_box_pothole_exist
     label = self.label_id       
