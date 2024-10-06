554b19db768490cfcf845874755778f986f9ebb8
sentulmari18@gmail.com <sentulmari18@gmail.com>
2024-10-06 04:57:29 +0000

    Edited Admin, Homepage, Layout, Review and 1 more

--- client_code/Homepage.py
+++ client_code/Homepage.py
@@ -23,34 +23,35 @@ class Homepage(HomepageTemplate):  # Your landing page form
   # Create an instance of the Layout form
     layout_form = Layout()
     # Load Formtest into the content slot of Layout
-    layout_form.load_child_form(Form1_copy.Form1_copy())
+    # layout_form.load_child_form(Form1_copy.Form1_copy())
+    layout_form.load_child_form(Form1.Form1())
     # Open the Layout form
     open_form(layout_form)
 
   def button_admin_click(self, **event_args):
     # Load AdminForm into Layout
     layout_form = Layout()  # Create an instance of the Layout form
-    layout_form.load_child_form(Admin())  # Load AdminForm into the content slot
+    layout_form.load_child_form(Admin.Admin())  # Load AdminForm into the content slot
     open_form(layout_form)  # Open the Layout form
 
   def button_dashboard_click(self, **event_args):
     # Load DashboardForm into Layout
     layout_form = Layout()  # Create an instance of the Layout form
-    layout_form.load_child_form(Stats())  # Load DashboardForm into the content slot
+    layout_form.load_child_form(Stats.Stats())  # Load DashboardForm into the content slot
     open_form(layout_form)  # Open the Layout form
 
   def button_review_click(self, **event_args):
     layout_form = Layout()
-    layout_form.load_child_form(Review())  
+    layout_form.load_child_form(Review.Review())  
     open_form(layout_form)
 
-  def link_demo_homepg_click(self, **event_args):
-    # Create an instance of the Layout form
-    layout_form = Layout()
-    # Load Form1 into the content slot of Layout
-    layout_form.load_child_form(Form1())
-    # Open the Layout form
-    open_form(layout_form)
+  # def link_demo_homepg_click(self, **event_args):
+  #   # Create an instance of the Layout form
+  #   layout_form = Layout()
+  #   # Load Form1 into the content slot of Layout
+  #   layout_form.load_child_form(Form1())
+  #   # Open the Layout form
+  #   open_form(layout_form)
 
 
 
